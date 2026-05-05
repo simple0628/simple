56f41d2d87ff965e30a54f5977295530d8bb3c0d
simple0628 <simple0628@users.noreply.github.com>
2026-05-05 18:16:55 +0800

    移除临时上传脚本

--- upload.py
+++
@@ -1,7 +0,0 @@
-import subprocess, getpass
-
-token = getpass.getpass("请粘贴你的 PyPI API Token: ")
-subprocess.run([
-    "python", "-m", "twine", "upload", "dist/*",
-    "-u", "__token__", "-p", token
-], shell=True, cwd="E:/shenmo/simple-code")
